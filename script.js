5fb754541b9654b5dd15bb19668b32a701c92382
Nico Cucciniello <ncucciniello@gmail.com>
2016-10-04 21:33:12 -0400

    add explosion

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 console.log('js is linked');
+$(function() {
+  // ship starts offscreen moves left to right
+  // till its offscreen and stops
+  function shipFlys() {
+    var $ship = $('.ship');
+    var distance = 1300;
+    setInterval(function(){
+      $ship.css('right', distance + 'px');
+      if (distance < -270) {
+      } else {
+        distance -= 10;
+        }
+    }, 50);
 
-function shipFlys() {
-  var $ship = $('.ship');
-  var distance = 1300;
-  setInterval(function(){
-    $ship.css('right', distance + 'px');
-    if (distance < -262) {
-    } else {
-      distance -= 10;
-    }
-  }, 40);
+  // // If ship makes it past console logs that you lost a life
+  // if ($ship.position().right <= -270) {
+  // console.log('lose a life');
+  //  };
 };
+  // When the ship is clicked it explodes and disappears
+  function shipExp(){
+    console.log('EXPLODE!');
+    $('.ship-img').attr('src', 'img/boom.png');
+  };
 
-$(document).ready(function(){
+// Fires the function for the ship to fly on page load.
+$(document).ready(function() {
   shipFlys();
+})
+
+//  When the ship is click it will fire the funtion that kills it.
+$('.ship').click(shipExp);
+
 })
