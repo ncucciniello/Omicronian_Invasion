4080fede3f30412d70aa9246244b106bb3a95ffe
Nico Cucciniello <ncucciniello@gmail.com>
2016-10-06 00:43:05 -0400

    add hud and ship disappear

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 console.log('js is linked');
 $(function() {
+  // Will create a new div including the ship and spawn it in a random row
+  // function spawnShip() {
+  //   var ship =
+  // }
+
   // ship starts offscreen moves left to right
   // till its offscreen and stops
   function shipFlys() {
     var $ship = $('.ship');
-    var distance = 1300;
+    var distance = 1320;
     setInterval(function(){
       $ship.css('right', distance + 'px');
-      if (distance < -270) {
+      if (distance < -240) {
       } else {
         distance -= 10;
         }
     }, 50);
-
-  // // If ship makes it past console logs that you lost a life
+  // If ship makes it past console logs that you lost a life
   // if ($ship.position().right <= -270) {
   // console.log('lose a life');
   //  };
-};
+  };
+
   // When the ship is clicked it explodes and disappears
   function shipExp(){
     console.log('EXPLODE!');
     $('.ship-img').attr('src', 'img/boom.png');
+    $('.ship').fadeOut(800);
+    // $('.ship').remove();
   };
 
-// Fires the function for the ship to fly on page load.
-$(document).ready(function() {
-  shipFlys();
-})
+  // Fires the function for the ship to fly on page load.
+  $(document).ready(function() {
+    shipFlys();
+  })
 
-//  When the ship is click it will fire the funtion that kills it.
-$('.ship').click(shipExp);
-
+  //  When the ship is click it will fire the funtion that kills it.
+  $('.ship').click(shipExp);
 })
