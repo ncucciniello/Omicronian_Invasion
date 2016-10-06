c83a92c7d63d862c9d612ce1f969adfe77a5e5d8
Nico Cucciniello <ncucciniello@gmail.com>
2016-10-06 14:10:30 -0400

    life is lost if ship is missed

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 console.log('js is linked');
 $(function() {
-  // // Will create a new div including the ship and spawn it in a random row
-  // // with a random speed.
-  // function spawnShip() {
-  // }
-
+var flyer = 0;
   // ship starts offscreen moves left to right
   function shipFlys() {
     var ship = $('.ship');
     var distance = -270;
-    setInterval(function(){
+    flyer = setInterval(function(){
       ship.css('left', distance + 'px');
       if (distance > $(window).width()) {
       } else {
         distance += 15;
         }
     }, 50);
-
-  // // If ship makes it past console logs that you lost a life
-  // var shipPos = $('.ship').position().left;
-  // var windowSize = $(window).width();
-  // if ( shipPos > windowSize-100) {
-  // console.log('lose a life');
-  //  }
   };
+
+  // when a ship makes it past you lose a life.
+  var lifeID = setInterval(function() {
+    var shipPos = $('.ship').position().left;
+    var windowSize = $(window).width();
+    if ( shipPos > windowSize-100) {
+      console.log('lose a life');
+      $('.lives img').last().remove();
+      clearInterval(lifeID)
+      }
+    }, 10);
 
   // When the ship is clicked it explodes, disappears,
   // and you will gain 10 pts per ship killed.
@@ -35,6 +35,8 @@ $(function() {
     $('.ship').delay( 5000, function(){
       $('.ship').remove();
       });
+    clearInterval(flyer);
+    clearInterval(lifeID);
 
     var currentVal = parseInt($('.score').text(),10);
     var newVal = currentVal + 10;
