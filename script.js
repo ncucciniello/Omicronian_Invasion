f0a64bf21997b31e7d698801338b76fd898132ae
Nico Cucciniello <ncucciniello@gmail.com>
2016-10-07 10:24:02 -0400

    things added

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 console.log('js is linked');
 $(function() {
-var flyer = 0;
-  // ship starts offscreen moves left to right
+
+  // global variables
+  var flyer = 0;
+  var ship = $('.ship');
+
+  // ship1 starts offscreen moves left to right
   function shipFlys() {
-    var ship = $('.ship');
     var distance = -270;
     flyer = setInterval(function(){
       ship.css('left', distance + 'px');
@@ -14,11 +17,16 @@ var flyer = 0;
     }, 50);
   };
 
+  // Fires the function for the ship1 to fly on page load.
+  $(document).ready(function() {
+    shipFlys();
+  });
+
   // when a ship makes it past you lose a life.
-  var lifeID = setInterval(function() {
-    var shipPos = $('.ship').position().left;
+    var lifeID = setInterval(function() {
+    var shipPos = ship.position().left;
     var windowSize = $(window).width();
-    if ( shipPos > windowSize-100) {
+    if (shipPos > windowSize) {
       console.log('lose a life');
       $('.lives img').last().remove();
       clearInterval(lifeID)
@@ -31,9 +39,9 @@ var flyer = 0;
     var currentPts = 0;
     console.log('EXPLODE!');
     $('.ship-img').attr('src', 'img/boom.png');
-    $('.ship').fadeOut(800);
-    $('.ship').delay( 5000, function(){
-      $('.ship').remove();
+    ship.fadeOut(800);
+    ship.delay( 5000, function(){
+      ship.remove();
       });
     clearInterval(flyer);
     clearInterval(lifeID);
@@ -43,12 +51,6 @@ var flyer = 0;
     $('.score').text(newVal);
   };
 
-
-  // Fires the function for the ship to fly on page load.
-  $(document).ready(function() {
-    shipFlys();
-  })
-
   //  When the ship is click it will fire the funtion to kill it.
-  $('.ship').click(shipExp);
+  ship.click(shipExp);
 })
