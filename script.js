3c69f859beb1dc795d9f3ad8c9f7b10c536a762c
Nico Cucciniello <ncucciniello@gmail.com>
2016-10-08 16:46:59 -0400

    add random number of ships spawned

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ console.log('js is linked');
 $(function() {
 
   // global variables
-  var flyer = 0;
   var ship = $('.ship');
 
-  // spawn ship
-  //// write if statment to keep spawning if lives > 0
+  // creates a new ship in a random location with a random speed
+  // and puts it into the html. If the ship makes it past the right
+  // boarder of the screen you lose a life.
   function spawnShip() {
     var newDiv = $('<div />').attr('class', 'ship').appendTo('main');
     var rndmNum = Math.floor((Math.random() * 500) + 25);
@@ -21,49 +21,20 @@ $(function() {
           console.log('lose a life');
           $('.lives img').last().remove();
           clearInterval(removeLife);
-        }
-      }.bind(newDiv), 50);
-  }
+          }
+    }.bind(newDiv), 50);
+  };
 
-
-  // // ship starts offscreen moves left to right
-  // function shipFlys() {
-  //   var distance = -270;
-  //   var rndmSpeed = Math.floor((Math.random() * 20) + 15);
-  //   for (var i = 0; i < ship.length; i++) {
-  //     setInterval(function(){
-  //       ship.css('left', distance + 'px');
-  //       if (distance > $(window).width()) {
-  //       } else {
-  //         distance += rndmSpeed;
-  //         }
-  //     }, 50);
-  //   }
-  // }
-
-  // // when a ship makes it past you lose a life.
-  // var lifeID = setInterval(function() {
-  //   var shipPos = $('.ship').position().left;
-  //   var windowSize = $(window).width();
-  //   if (shipPos > windowSize) {
-  //     console.log('lose a life');
-  //     $('.lives img').last().remove();
-  //     lives--;
-  //     clearInterval(lifeID)
-  //     }
-  // }, 10);
-
-  // When the ship is clicked it explodes, disappears, and you will gain 10 pts per ship killed.
+  // when the ship is clicked it will explode, disappear, and you will
+  // gain 10 pts per ship killed.
   function shipExp(){
-    var currentPts = 0;
+    // var currentPts = 0;
     console.log('EXPLODE!');
-    ship.css('background','url(img/boom.png)');
-    ship.fadeOut(800);
-    ship.delay( 5000, function(){
-      ship.remove();
-      });
-    clearInterval(flyer);
-    clearInterval(lifeID);
+    // $(this).css('background','url(img/boom.png)');
+    // $(this).fadeOut(800);
+    // $(this).delay( 5000, function(){
+    //   $(this).remove();
+    //   });
 
     var currentVal = parseInt($('.score').text(),10);
     var newVal = currentVal + 10;
@@ -71,21 +42,27 @@ $(function() {
   };
 
   // Life tracker
+  var lives = $('.life').length;
+  if (lives === 0) {
+    alert('You have lost!');
+  };
 
 
 
   //EVENT CALLERS
 
-  // Fires the function for the ship1 to fly on page load.
+  // Fires the function for the ship to fly on page load.
   $(document).ready(function() {
     setInterval(function () {
       var counter = 0
-      while (counter < 1) {
+      var numOfShips = Math.floor((Math.random() * 3) + 1);
+      while (counter < numOfShips) {
         spawnShip();
         counter++
       }
      }, 2000);
+
   //  When the ship is click it will fire the funtion to kill it.
-    ship.click(shipExp);
+    $('main').click(shipExp);
   });
 });
